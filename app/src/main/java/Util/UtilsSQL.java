462be937a7cf10bc162860c806901aa30a37b998
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2022-09-25 20:30:42 -0300

    Alta de usuario

--- app/src/main/java/Util/UtilsSQL.java
+++ app/src/main/java/Util/UtilsSQL.java
@@ -1,6 +1,8 @@
 package Util;
 
 public class UtilsSQL {
-    public static String querty_create_Usuarios = "create table usuarios(ID integer primary key autoincrement, Nombre text, Correo text, Contrasenia text)";
-    public static String query_login = "select ID,Nombre from usuarios where Correo = '%1$s' and Contrasenia = '%2$s'";
+    public static String QUERY_CREATE_USUARIOS = "create table usuarios(ID integer primary key autoincrement, Nombre text, Correo text, Contrasenia text)";
+    public static String QUERY_LOGIN = "select ID,Nombre from usuarios where Correo = '%1$s' and Contrasenia = '%2$s'";
+    public static String SELECT_USUARIO_BY_NAME = "select ID from usuarios where Nombre = ?";
+    public static String SELECT_USUARIO_BY_MAIL = "select ID from usuarios where Correo = ?";
 }
